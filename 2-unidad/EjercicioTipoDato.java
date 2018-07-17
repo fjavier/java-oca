4d5044db43d60b73465ff75a25499a78a5e33701
cisco505 <fjavier.cgam@gmail.com>
2018-07-16 19:16:54 -0600

    Jugando con tipos de datos en Java

--- 2-unidad/EjercicioTipoDato.java
+++ 2-unidad/EjercicioTipoDato.java
@@ -13,5 +13,47 @@ public class EjercicioTipoDato{
 		System.out.println(n);
 		System.out.println(c);
 		System.out.println(f);
+
+		System.out.println("=========");
+		leerByte();
+		overflowFlotante();
+		operacionCharacter();
+		promocionarOperacion();
+
+	}
+
+	static void leerByte(){ 
+		byte a = 0;
+		for(int i = 0; i < 256 ; i++){
+		    a = (byte)i;
+		    //Luego de alcanzar 127 el tipo de dato hace un overflow, lo cual es que 
+		    //Despues de alcanzar su punto maximo, devuelve -128 su punto minimo ;)
+		    System.out.println(a);
+		}
+		System.out.println(a);	
+	}
+
+	static void overflowFlotante(){
+		System.out.println("==OB=======Overflow flotante=========");
+		//Esta operación generara un valor Infinito :O
+		float f = 3.4028235E38f * 20f;
+		System.out.println(f);
+	}
+
+	static void operacionCharacter(){
+		System.out.println("===========OPERACION CHARACTER=============");
+		char a = 'a';
+		char b = 'b';
+		int c = a + b;
+		System.out.println((byte)a);
+	}
+
+	static void promocionarOperacion(){
+		System.out.println("=============PromocionarOperacion=========");
+		int a = (byte)'a';
+		float b = a;
+		double n = 123.23f;
+		double x = a + b + n;
+		System.out.println(a+b+n);
 	}
 }
